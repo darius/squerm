eadbfead065f2ff510169ac36894cc146a7e4007
darius <darius@66-214-143-69.dhcp.gldl.ca.charter.com>
2008-05-18 22:46:26 -0700

    remove unused code

--- clutch.py
+++ clutch.py
@@ -13,22 +13,3 @@ class Box:
         self._ = value
     def __repr__(self):
         return 'Box(%r)' % self._
-
-class Struct:
-    """Create an instance with argument=value slots.
-    This is for making a lightweight object whose class doesn't matter."""
-    def __init__(self, **entries):
-        self.__dict__.update(entries)
-    def __cmp__(self, other):
-        if isinstance(other, Struct):
-            return cmp(self.__dict__, other.__dict__)
-        else:
-            return cmp(self.__dict__, other)
-    def __repr__(self):
-        return 'Struct(%s)' % ', '.join('%s=%r' % item 
-                                        for item in vars(self).items())
-
-def initialize(entries):
-    entries['self'].__dict__.update((key, value) 
-                                    for key, value in entries.items()
-                                    if key != 'self')
