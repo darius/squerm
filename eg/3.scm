073a7c4f58502492592219010091dcc3bbadcbfd
darius <darius@24-205-38-116.dhcp.gldl.ca.charter.com>
2008-05-21 15:49:27 -0700

    eval example

---
+++ eg/3.scm
@@ -0,0 +1,15 @@
+(define (main)
+  (print (eval '(cons 'a '()) safe-environment))
+  (print (eval (test1) safe-environment)))
+
+(define (print x)
+  (write x)
+  (newline))
+
+(define (test1)
+  '(local 
+    ((define (revappend xs ys)
+       (if (null? xs)
+	   ys
+	   (revappend (cdr xs) (cons (car xs) ys)))))
+    (revappend '(a b c) '(d))))
